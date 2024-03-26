a39eebb328eb96e3e92c69bfc93b7d9b4707b453
Malcolm <137590820+HydrogenOS1@users.noreply.github.com>
2024-03-26 10:02:57 +1000

    Upload V1.2.1-BATA

--- SlicerStaffCommands/src/main/java/com/malcolmjh/slicerstaffcommands/SlicerStaffCommands.java
+++ SlicerStaffCommands/src/main/java/com/malcolmjh/slicerstaffcommands/SlicerStaffCommands.java
@@ -6,24 +6,19 @@ import com.malcolmjh.slicerstaffcommands.commands.spawnCommand;
 import com.malcolmjh.slicerstaffcommands.commands.setSpawnCommand;
 import com.malcolmjh.slicerstaffcommands.listeners.joinLeaveListener;
 import com.malcolmjh.slicerstaffcommands.listeners.spawnListener;
+import org.bukkit.ChatColor;
 import org.bukkit.plugin.java.JavaPlugin;
 public final class SlicerStaffCommands extends JavaPlugin {
     @Override
     public void onEnable() {
-        getLogger().info("[START UP-LOADING] [INFO]: Slicer Slices Is Loading...");
         getConfig().options().copyDefaults();
         saveDefaultConfig();
-        getLogger().info("[START UP-1] [COMMANDS] [INFO]: The Commands Are Starting...");
         getCommand("hologram").setExecutor(new hologramCommand());
         getCommand("invsee").setExecutor(new invseeCommand());
         getCommand("notice").setExecutor(new noticeCommand(this));
         getCommand("setspawn").setExecutor(new setSpawnCommand(this));
         getCommand("spawn").setExecutor(new spawnCommand(this));
-        getLogger().info("[START UP-2] [COMMANDS] [INFO]: The Commands Have Started!");
-        getLogger().info("[START UP-3] [COMMANDS] [INFO]: The Commands Are Starting...");
         getServer().getPluginManager().registerEvents(new spawnListener(this), this);
         getServer().getPluginManager().registerEvents(new joinLeaveListener(this), this);
-        getLogger().info("[START UP-4] [COMMANDS] [INFO]: The Commands Have Started!");
-        getLogger().info("[START UP-FINAL] [INFO]: Slicer Slices Has Started!");
     }
 }
